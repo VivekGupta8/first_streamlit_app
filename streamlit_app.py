cd36ffa4dbab450e9c770a3fff7193653ba216ca
Vivek Gupta <113456170+VivekGupta8@users.noreply.github.com>
2023-09-24 13:43:28 +0530

    Update streamlit_app.py
    Typo error solve

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 streamlit.title('My Portfolio')
 streamlit.header('My Services')
 streamlit.text('Data Analysis')
-Streamlit.text('Data Visualization')
+streamlit.text('Data Visualization')
 streamlit.text('Business Intelligence')
 streamlit.text('power BI consultancy')
-Streamlit.header('At cost-effective price @$5/hr')
