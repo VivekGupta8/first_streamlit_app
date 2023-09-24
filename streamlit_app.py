381a96d3064e03af501ebc63d9b4440e1e9d2d4c
Vivek Gupta <113456170+VivekGupta8@users.noreply.github.com>
2023-09-24 13:51:40 +0530

    Update streamlit_app.py
    New header contract type and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('power BI consultancy')
 
 streamlit.header('Price')
 streamlit.text('Go ahead and shoot me a quick message here- mba.gupta.vivek@gmail.com')
+
+streamlit.header('Contract type')
+streamlit.text('Monthly')
+streamlit.text('Yearly')
+streamlit.text('Hourly')
