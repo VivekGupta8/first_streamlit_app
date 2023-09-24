5da50a710b592ce912613bd0b27f571747dd09f5
Vivek Gupta <113456170+VivekGupta8@users.noreply.github.com>
2023-09-24 13:42:14 +0530

    Update streamlit_app.py
    Updated the title and added the content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('my services')
+streamlit.title('My Portfolio')
+streamlit.header('My Services')
+streamlit.text('Data Analysis')
+Streamlit.text('Data Visualization')
+streamlit.text('Business Intelligence')
+streamlit.text('power BI consultancy')
+Streamlit.header('At cost-effective price @$5/hr')
