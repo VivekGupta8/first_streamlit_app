3896793f8ba8c1fac3bf7f93836967d70d813a1b
Vivek Gupta <113456170+VivekGupta8@users.noreply.github.com>
2023-09-26 10:41:05 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ streamlit.dataframe(fruits_to_show)
 #creating a function
 def get_fruityvice_data (this_fruit_choice):
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized
 
 #25-09
 #FruityVice API
