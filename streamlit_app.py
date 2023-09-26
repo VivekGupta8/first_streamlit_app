29b0661bfc0753464e94567e178de985cb8ba4ca
Vivek Gupta <113456170+VivekGupta8@users.noreply.github.com>
2023-09-26 11:41:15 +0530

    Update streamlit_app.py
    Close button

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,9 +48,10 @@ def get_fruit_load_list():
           my_cur.execute("select * from fruit_load_list")
           return my_cur.fetchall()
 #add button
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get fruit list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      my_data_rows = get_fruit_load_list()
+     my_cnx.close()
      streamlit.dataframe(my_data_rows)
 #stop
 #except URLError as e:
@@ -66,6 +67,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button ('Add a fruit to the list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      back_from_function = insert_row_snowflake(add_my_fruit)
+     my_cnx.close()
      streamlit.text(back_from_function)
 #streamlit.write('Thanks for adding', add_my_fruit)
 
